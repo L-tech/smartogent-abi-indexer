98e9ecfdb1fdcadb78f1fa12ac434b498c9f570e
L-tech <legendabrahamonoja@gmail.com>
2024-06-06 13:52:25 +0100

    Switch from lighhouse to nft storage

--- knowledgebase/upload_documents_use_case.py
+++ knowledgebase/upload_documents_use_case.py
@@ -1,6 +1,7 @@
 import json
 import os
 import uuid
+import requests
 import settings
 from typing import List
 from langchain.schema import Document
@@ -11,31 +12,19 @@ def execute(documents: List[Document]) -> str:
     # Serialize the documents
     serialized_data = _serialize_documents(documents)
     
-    # Generate a unique file name
-    unique_id = uuid.uuid4().hex[:8]
-    file_name = f"SmartoGent_{unique_id}.json"
-    
-    # Ensure the uploads directory exists
-    uploads_dir = "uploads"
-    os.makedirs(uploads_dir, exist_ok=True)
-    
-    # Create the full file path
-    file_path = os.path.join(uploads_dir, file_name)
-    
-    # Store the serialized data as a .json file
-    with open(file_path, 'w') as json_file:
-        json_file.write(serialized_data)
-    
-    # Initialize the Lighthouse instance
-    lh = Lighthouse(token=settings.STORAGE_KEY)
-    
-    # Upload the .json file
-    upload_response = lh.upload(source=file_path)
-    
-    # Extract the hash from the upload response
-    file_hash = upload_response['data']['Hash']
-    
-    return file_hash
+    serialized_data = _serialize_documents(documents)
+    multipart_data = {
+        "file": ("file", serialized_data, ),
+    }
+    response = requests.post(
+        "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        headers={
+            "Authorization": f"Bearer {settings.STORAGE_KEY}",
+        },
+        files=multipart_data,
+    )
+    response.raise_for_status()
+    return response.json().get("IpfsHash")
 
 
 def _serialize_documents(documents: List[Document]) -> str:
